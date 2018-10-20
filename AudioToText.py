09ba7e1011dfd9eb53bc5733659c258d1529e67f
fmsuvM <k222.tkd.fms@gmail.com>
2018-10-20 15:58:13 +0900

    Feat: add narration to text function
    音声をテキストに変換

---
+++ AudioToText.py
@@ -0,0 +1,30 @@
+import io
+import os
+from google.cloud import speech
+from google.cloud.speech import enums, types
+
+
+def TranscriptNarration(audiofile=''):
+    if not audiofile:
+        return None
+    client = speech.SpeechClient()  # initialize client
+    filename = os.path.join(
+        os.path.dirname(__file__),
+        'uploads',
+        audiofile
+    )
+
+    with io.open(filename, 'rb') as audiofile:
+        content = audiofile.read()
+        audio = types.RecognitionAudio(content=content)
+
+    config = types.RecognitionConfig(
+        encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=16000,
+        language_code='ja-JP'
+    )
+
+    response = client.recognize(config, audio)
+    for result in response.results:
+        narration_text = result.alternatives[0].transcript
+    return narration_text
---
+++ index.py
@@ -0,0 +1,48 @@
+import os
+from flask import Flask, flash, request, redirect, url_for, jsonify
+from werkzeug.utils import secure_filename
+from Analyzer import AnalyzedDescription
+from AudioToText import TranscriptNarration
+
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = set(
+    ['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'mp4', 'raw'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['JSON_AS_ASCII'] = False
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/')
+def route():
+    return 'root dir'
+
+
+@app.route('/analyze', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(filepath)
+            _transcript = TranscriptNarration(audiofile=filename)
+            _result = AnalyzedDescription(_transcript)
+            result = {
+                'data': _result
+            }
+            return jsonify(result)
+
+
+# app.run(host="127.0.0.1", port=5000, debug=True)
