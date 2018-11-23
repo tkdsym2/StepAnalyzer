c489709e6c644ce2dbc2fcd4929af6915f491915
fmsuvM <k222.tkd.fms@gmail.com>
2018-11-23 12:49:39 +0900

    Feat: update so path

--- Yolo.py
+++ Yolo.py
@@ -53,7 +53,7 @@ class METADATA(Structure):
 
 
 #lib = CDLL("/home/pjreddie/documents/darknet/libdarknet.so", RTLD_GLOBAL)
-lib = CDLL("libdarknet.so", RTLD_GLOBAL)
+lib = CDLL("/home/tkd/workspace/darknet/libdarknet.so", RTLD_GLOBAL)
 lib.network_width.argtypes = [c_void_p]
 lib.network_width.restype = c_int
 lib.network_height.argtypes = [c_void_p]
